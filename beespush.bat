2c5ff2bf7cb752ef51b1a0b313cdcfbf749f7fb4
jupi <ungvari.mail@gmail.com>
2013-06-06 09:06:55 +0200

    cloudbees fix

--- beespush.bat
+++ beespush.bat
@@ -1,4 +1,5 @@
 @echo off
 call play dist
-call bees app:deploy -a aego -t play2 -P config.resource=dist.cloudbees.conf -P file.separator=\/ dist\aego-play-1.0-SNAPSHOT.zip
+call bees app:deploy -Rjava_version=1.7 -a aego -t play2 -P config.resource=dist.cloudbees.conf -P file.separator=\/ dist\aego-play-1.0-SNAPSHOT.zip
+call bees app:update aego disableProxyBuffering=true
 rem bees config:set -a aego config.resource=dist.cloudbees.conf
